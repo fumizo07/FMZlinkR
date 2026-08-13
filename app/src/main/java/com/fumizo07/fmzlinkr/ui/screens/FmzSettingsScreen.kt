a303173ab9c000c2c78073d5d1fadd8c6e26461d
fumizo07 <87250591+fumizo07@users.noreply.github.com>
2026-08-13 13:20:17 +0900

    Update FmzSettingsScreen.kt

--- app/src/main/java/com/fumizo07/fmzlinkr/ui/screens/FmzSettingsScreen.kt
+++ app/src/main/java/com/fumizo07/fmzlinkr/ui/screens/FmzSettingsScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
